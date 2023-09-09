2d1bff201c0c5750dc582875b3dc365f53328306
Henrik Christensen <hdc@mjolner.dk>
2023-09-09 20:25:18 +0200

    commit

--- src/bernoulli.py
+++ src/bernoulli.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from math import comb
 
 
 class BernoulliExperiment:
@@ -13,15 +14,15 @@ class BernoulliExperiment:
         self.markov_bounds = []
         self.chebyshev_bounds = []
         self.hoeffding_bounds = []
+        self.num_trials = 20
 
     def run_experiment(self):
         # Number of repetitions
         num_repetitions = 1000000
         # Number of Bernoulli trials in each experiment
-        num_trials = 20
 
         for alpha in self.alphas:
-            experiment_results = np.random.binomial(1, self.bias, size=(num_repetitions, num_trials))
+            experiment_results = np.random.binomial(1, self.bias, size=(num_repetitions, self.num_trials))
 
             # Calculate mean
             mean_results = np.mean(experiment_results, axis=1)
@@ -32,18 +33,33 @@ class BernoulliExperiment:
             self.empirical_frequencies.append(empirical_frequency)
 
             # Calculate Markov's bound
-            expected_value = np.mean(mean_results)
-            markov_bound = expected_value / alpha
+            # P(X >= alpha) <= E[X] / alpha
+            markov_bound = np.mean(mean_results) / alpha
             self.markov_bounds.append(min(markov_bound, 1))  # ensure the bound is <= 1
 
             # Calculate Chebyshev's bound
-            k = np.abs(alpha - np.mean(mean_results)) / np.sqrt(np.var(mean_results))
-            chebyshev_bound = 1 / (k**2)
-            self.chebyshev_bounds.append(min(chebyshev_bound, 1))  # ensure the bound is <= 1
+            # P(|X - E[X]| >= k) <= Var(X) / k^2
+            # Where k is the distance from the mean (i.e. standard deviation)
+            # k = |alpha - E[X]|
+            var_X = np.var(mean_results)
+            k = np.abs(alpha - np.mean(mean_results))
+            cheby_bound = var_X / k**2
+            self.chebyshev_bounds.append(min(cheby_bound, 1))  # ensure the bound is <= 1
 
             # Calculate Hoeffding's bound
-            hoeffding_bound = np.exp(-2 * num_trials * (alpha - 0.5) ** 2)
-            self.hoeffding_bounds.append(hoeffding_bound)
+            # P(|X - E[X]| >= k) <= 2 * exp(-2 * k^2 * n)
+            # Where k is the distance from the mean (i.e. standard deviation)
+            # k = |alpha - E[X]|
+            k = np.abs(alpha - np.mean(mean_results))
+            hoeffding_bound = 2 * np.exp(-2 * k**2 * self.num_trials)
+            self.hoeffding_bounds.append(min(hoeffding_bound, 1))  # ensure the bound is <= 1
+
+    def exact_probability(self, alpha):
+        threshold = int(self.num_trials * alpha)
+        prob = sum(
+            comb(self.num_trials, k) * self.bias**k * (1 - self.bias) ** (self.num_trials - k) for k in range(threshold, self.num_trials + 1)
+        )
+        return prob
 
     def report(self):
         for alpha, emp_freq, markov, cheby, hoeff in zip(
@@ -56,6 +72,9 @@ class BernoulliExperiment:
             print(f"Hoeffding's Bound: {hoeff}")
             print("-" * 50)
 
+        print(f"Exact Probability: {self.exact_probability(0.95)}")
+        print(f"Exact Probability: {self.exact_probability(1)}")
+
     def plot(self):
         plt.figure(figsize=(10, 6))
         plt.plot(self.alphas, self.empirical_frequencies, marker="o", linestyle="-", label="Empirical Frequency")
@@ -64,7 +83,8 @@ class BernoulliExperiment:
         plt.plot(self.alphas, self.hoeffding_bounds, marker="^", linestyle=":", label="Hoeffding's Bound")
         plt.xlabel("Alpha")
         plt.ylabel("Frequency / Bound")
-        plt.title("Empirical Frequency vs. Bounds (Bounded at 1)")
+        plt.title("Empirical Frequency vs. Bounds")
         plt.legend()
         plt.grid(True)
+        plt.tight_layout()
         plt.show()
--- src/linear_regression.py
+++ src/linear_regression.py
@@ -14,7 +14,7 @@ class LinearRegression:
         self.X = data[:, 0][sorted_indices]
         self.y_true = data[:, 1][sorted_indices]
 
-        # Construct the data set S'
+        # Construct data set S'
         self.X_prime = X_map_func(self.X) if X_map_func else self.X
         self.y_prime = np.log(self.y_true)
 
@@ -22,39 +22,38 @@ class LinearRegression:
         # Append a column of ones to X
         X_tilde = np.c_[np.ones(self.X_prime.shape[0]), self.X_prime]
 
-        # Calculate w = (X^T * X)^-1 * X^T * y (normal equation)
-        w = np.linalg.inv(X_tilde.T.dot(X_tilde)).dot(X_tilde.T).dot(self.y_prime)
+        # Calculate (X^T * X)^-1 * X^T * y and obtain parameters a and b
+        self.b, self.a = np.linalg.inv(X_tilde.T.dot(X_tilde)).dot(X_tilde.T).dot(self.y_prime)
 
-        # Extract parameters a and b
-        self.b, self.a = w
-
-        # Fit the model
+        # Fit model h'(x) = a * x + b
         h_prime = self.a * self.X_prime + self.b
 
-        # Obtain final model h(x)=exp(h_prime(x)) (i.e. predictions)
+        # Obtain final model h(x) = exp(h'(x)) (i.e. predictions)
         self.y_pred = np.exp(h_prime)
 
-    def mean_squared_error(self):
+    def mse(self):
+        # MSE = 1/n * sum((y - h(x))^2)
         return np.mean((self.y_true - self.y_pred) ** 2)
 
-    def r_squared(self):
+    def r2(self):
+        # R^2 = 1 - sum((y - h(x))^2) / sum((y - y_bar)^2)
         y_bar = np.mean(self.y_true)
-
         return 1 - (np.sum((self.y_true - self.y_pred) ** 2) / np.sum((self.y_true - y_bar) ** 2))
 
     def report(self):
-        print(f"Parameter a: {self.a}")
-        print(f"Parameter b: {self.b}")
-        print(f"Mean squared error: {self.mean_squared_error()}")
-        print(f"R^2: {self.r_squared()}")
+        print(f"a: {self.a}")
+        print(f"b: {self.b}")
+        print(f"MSE: {self.mse()}")
+        print(f"R^2: {self.r2()}")
 
     def plot(self):
-        # always use "normal" X scale
+        # Always use "normal" X scale
         plt.scatter(self.X, self.y_true, color="b", label="True values")
         plt.scatter(self.X, self.y_pred, color="r", marker="x", label="Predictions")
 
         plt.xlabel("Age of the fish")
-        plt.ylabel("PCB concentration")
-        plt.legend()
+        plt.ylabel("Log of PCB concentration")
         plt.yscale("log")
+        plt.legend()
+        plt.tight_layout()
         plt.show()
